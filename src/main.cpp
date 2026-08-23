65dcd277e89c497beac9f81537bc3ba64e29ad4d
Hemant Bhandari <zenithtaiga@gmail.com>
2026-08-23 21:55:22 +0530

    Change key press handling
    - Quit: ctrl (left/right) + Q
    - Toggle Polygon: ctrl (left/right) + P

--- src/main.cpp
+++ src/main.cpp
@@ -163,13 +163,19 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 
 void processInput(GLFWwindow* window) {
 	static bool key_p_ispress = false;
+	static bool key_ctrl_ispress = false;
 
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+	if ((glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) ||
+		(glfwGetKey(window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS)) {
+		key_ctrl_ispress = true;
+	}
+
+	if (key_ctrl_ispress && (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)) {
 		glfwSetWindowShouldClose(window, true);
 	}
 
 	// enter only if key was not pressed before
-	if (!key_p_ispress && (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)) {
+	if (key_ctrl_ispress && !key_p_ispress && (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)) {
 		toggle_glPolygonMode();
 		key_p_ispress = true; // now key is in "pressing" state
 		// don't enter if-block again till the key is released
