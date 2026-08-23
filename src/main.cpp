444ed604213265f3ee0784f5828484cd6ab3be9a
Hemant Bhandari <zenithtaiga@gmail.com>
2026-08-23 23:18:23 +0530

    restructure code

--- src/main.cpp
+++ src/main.cpp
@@ -3,97 +3,16 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include "toggle-polygon-mode.hpp"
-
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
-
-const char* vertexShaderSource = R"(
-#version 330 core
-layout (location = 0) in vec3 aPos;
-
-void main() {
-	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-}
-)";
-
-const char* fragmentShaderSource = R"(
-#version 330 core
-out vec4 FragColor;
-
-void main() {
-	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-}
-)";
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
+#include "init.hpp"
+#include "input.hpp"
+#include "render-state.hpp"
+#include "shader.hpp"
 
 
 int main() {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "LearnOpenGL", NULL, NULL);
-
-	if (window == NULL) {
-		std::cerr << "Failed to create GLFW widnow";
-		glfwTerminate();
-		return -1;
-	}
-
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cerr << "Failed to initialize GLAD";
-		return -1;
-	}
-
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	GLint success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-	if (!success) {
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
-			<< infoLog;
-	}
-
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-
-	if (!success) {
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
-			<< infoLog;
-	}
-
-	// shader program
-	unsigned int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cerr << "ERROR:SHADER::PROGRAM::LINKING_FAILED\n"
-			<< infoLog;
-	}
-
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	
+	GLFWwindow* window = initWindow();
+	unsigned int shaderProgram = createShaderProgram();
 	
 	// vertex data
 	float vertices[] = {
@@ -139,8 +58,8 @@ int main() {
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// check and call events and swap buffers
 		glfwSwapBuffers(window);
@@ -154,34 +73,4 @@ int main() {
 
 	glfwTerminate();
 	return 0;
-}
-
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow* window) {
-	static bool key_p_ispress = false;
-	static bool key_ctrl_ispress = false;
-
-	if ((glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) ||
-		(glfwGetKey(window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS)) {
-		key_ctrl_ispress = true;
-	}
-
-	if (key_ctrl_ispress && (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)) {
-		glfwSetWindowShouldClose(window, true);
-	}
-
-	// enter only if key was not pressed before
-	if (key_ctrl_ispress && !key_p_ispress && (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)) {
-		toggle_glPolygonMode();
-		key_p_ispress = true; // now key is in "pressing" state
-		// don't enter if-block again till the key is released
-	}
-
-	if ((glfwGetKey(window, GLFW_KEY_P) == GLFW_RELEASE)) {
-		key_p_ispress = false; // key no longer pressed
-	}
 }
